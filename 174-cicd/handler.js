39a557245b332b97ae0886ad1a0fc61c4b96f0bd
dongsj <667976dsa>
2019-11-29 14:33:03 +0800

    finish 197

---
+++ 174-cicd/handler.js
@@ -0,0 +1,15 @@
+module.exports.logger = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+        {
+          message: 'Serverless CI/CD Demo',
+          version: "v1.0",
+          timestamp: +new Date()
+        }
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
